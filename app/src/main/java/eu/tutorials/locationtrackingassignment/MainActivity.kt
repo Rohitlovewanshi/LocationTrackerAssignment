25b14d5fcb943a64ff07e97b8661890a162881ac
Rohitlovewanshi <rohitlovewanshi78@gmail.com>
2024-01-31 17:49:36 +0530

    initial commit

--- app/src/main/java/eu/tutorials/locationtrackingassignment/MainActivity.kt
+++ app/src/main/java/eu/tutorials/locationtrackingassignment/MainActivity.kt
@@ -27,7 +27,6 @@ class MainActivity : ComponentActivity() {
     private lateinit var mFusedLocationClient : FusedLocationProviderClient
     private lateinit var locationCallback: LocationCallback
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
