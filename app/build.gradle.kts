b8a9d42f83b0719bb94f483299d514acac5332ed
Rohitlovewanshi <rohitlovewanshi78@gmail.com>
2024-01-31 21:54:21 +0530

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,7 +51,6 @@ android {
 
 dependencies {
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.6")
     implementation("com.google.code.gson:gson:2.10")
 
